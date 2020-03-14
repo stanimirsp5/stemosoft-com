2ecd75272e8919c4bedda8630ff962a9485530b4
Belioff <emilbeliov@gmail.com>
2020-03-14 16:11:18 +0200

    Fix gulp build script to support nested templates

--- gulp_tasks/docs/views.js
+++ gulp_tasks/docs/views.js
@@ -7,7 +7,8 @@ const log = require('fancy-log');
 const replace = require('gulp-replace-path');
 
 gulp.task('docs_views',function(){
-	return gulp.src('src/docs/*.ejs')
+	return gulp.src(['src/docs/*.ejs', 'src/docs/privacyPolicies/*.ejs'])
+	.pipe((replace(/href\s*=\s*(['"])\/(.*?)(['"])/g,'href="./$2.html"')))
 	.pipe(ejs({ site_analytics: process.env.ANALYTICS || "",asset: function(assetLoc){ return assetLoc; }, convertType: ".html"}, {}, { ext: '.html' }).on('error', log))
 	.pipe(gulp.dest('./docs'))
 });
