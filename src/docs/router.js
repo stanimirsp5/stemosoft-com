dc67d61dd847d3bae3304192bd5981bfb355c816
Stan <stanimir.sp5@gmail.com>
2020-10-25 13:56:26 +0200

    add covid info privacy policy

--- src/docs/router.js
+++ src/docs/router.js
@@ -6,6 +6,7 @@ docsRouter.get('/', (req,res) => res.render('index'));
 docsRouter.get('/liveTextScreen', (req,res) => res.render('./privacyPolicies/liveTextScreen'));
 docsRouter.get('/airSofiaScreen', (req,res) => res.render('./privacyPolicies/airSofiaScreen'));
 docsRouter.get('/greenTicketScreen', (req,res) => res.render('./privacyPolicies/greenTicketScreen'));
+docsRouter.get('/covidInfoScreen', (req,res) => res.render('./privacyPolicies/covidInfoScreen'));
 // docsRouter.get('/elements', (req,res) => res.render('elements'));
 // docsRouter.get('/utilities', (req,res) => res.render('utilities'));
 // docsRouter.get('/additional', (req,res) => res.render('additional'));
