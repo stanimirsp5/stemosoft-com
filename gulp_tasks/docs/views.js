e45e7ddf995eb7fe2ab95dd532a3ff648c57f72f
Stanimir Petrov <stanimir.sp5@gmail.com>
2026-02-21 18:17:27 +0200

    Fix Pages build to output html files

--- gulp_tasks/docs/views.js
+++ gulp_tasks/docs/views.js
@@ -5,10 +5,12 @@ const gulp = require('gulp');
 const ejs = require('gulp-ejs');
 const log = require('fancy-log');
 const replace = require('gulp-replace-path');
+const rename = require('gulp-rename');
 
 gulp.task('docs_views',function(){
 	return gulp.src(['src/docs/*.ejs', 'src/docs/privacyPolicies/*.ejs'])
 	.pipe((replace(/href\s*=\s*(['"])\/(.*?)(['"])/g,'href="./$2.html"')))
 	.pipe(ejs({ site_analytics: process.env.ANALYTICS || "",asset: function(assetLoc){ return assetLoc; }, convertType: ".html"}, {}, { ext: '.html' }).on('error', log))
+	.pipe(rename({ extname: '.html' }))
 	.pipe(gulp.dest('./docs'))
 });
